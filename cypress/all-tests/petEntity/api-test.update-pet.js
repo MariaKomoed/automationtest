7a18e56a0b4018b074a86a8c48d813b87ca02524
Maria Komoed <mashshshk@gmail.com>
2019-10-24 18:13:16 +0300

    added api-tests

--- cypress/all-tests/petEntity/api-test.update-pet.js
+++ cypress/all-tests/petEntity/api-test.update-pet.js
@@ -50,6 +50,7 @@ describe('Tests for Update pet endpoint C37', () => {
         updatePet(requestData, false).then(response => {
             expect(response.status).to.eq(404);
             expect(response.status).to.eq('Not found');
+            cy.writeFile("body.txt", response);
         })
 
     })
@@ -101,6 +102,45 @@ describe('Tests for Update pet endpoint C37', () => {
         updatePet(requestData, false).then(response => {
             expect(response.status).to.eq(400);
             expect(response.message).to.eq('Bad Request');
+        })
+    })
+
+    it('Negative: Length of name field exceeds the max value C23', () => {
+        let dataSet=getPetRequestData(PET_LIMIT, true);
+        dataSet.name= Chance().string({length: PET_LIMIT.name.max + 1});
+        createPet(dataSet, false).then(response => {
+            expect(response.status).to.eq(400);
+            expect(response.messages[0].fieldName).to.eq(`${PET_LIMIT.name}`);
+            expect(response.messages[0].fieldError).to.eq(`Length must be between ${PET_LIMIT.name.min} and ${PET_LIMIT.name.max}`);
+        })
+    });
+
+    it('Negative: Length of category field exceeds the max value C24', () => {
+        let dataSet=getPetRequestData(PET_LIMIT, true);
+        dataSet.category.name = Chance().string({length: PET_LIMIT.category.name.max + 1});
+
+        createPet(dataSet, false).then(response => {
+            expect(response.status).to.eq(400);
+            expect(response.messages[0].fieldName).to.eq(`${PET_LIMIT.category.name}`);
+            expect(response.messages[0].fieldError).to.eq(`Length must be between ${PET_LIMIT.category.name.min} and ${PET_LIMIT.category.name.max}`);
+        })
+    });
+    it('Negative: Length of photoUrls field exceeds the max value C25', () => {
+        let dataSet=getPetRequestData(PET_LIMIT, true);
+        dataSet.photoUrls=fillUrls(PET_LIMIT.photoUrls.urlCount.max+1);
+        createPet(dataSet, false).then(response => {
+            expect(response.status).to.eq(400);
+            expect(response.messages[0].fieldName).to.eq(`${PET_LIMIT.photoUrls.urlCount}`);
+            expect(response.messages[0].fieldError).to.eq(`Length must be between ${PET_LIMIT.photoUrls.urlCount.min} and ${PET_LIMIT.photoUrls.urlCount.max}`);
+        })
+    })
+    it('Negative: Length of tags field exceeds the max value C26', () => {
+        let dataSet=getPetRequestData(PET_LIMIT, true);
+        dataSet.tags=fillTags(PET_LIMIT.tags.name.max+1)
+        createPet(dataSet, false).then(response => {
+            expect(response.status).to.eq(400);
+            expect(response.messages[0].fieldName).to.eq(`${PET_LIMIT.tags.name}`);
+            expect(response.messages[0].fieldError).to.eq(`Length must be between ${PET_LIMIT.tags.name.min} and ${PET_LIMIT.tags.name.max}`);
         })
     })
 });
